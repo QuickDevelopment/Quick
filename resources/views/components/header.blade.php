1617e9237f9b50b18af7fe662aef75c55ddeb16f
Keagan <keygun2k1@gmail.com>
2023-12-18 14:30:15 +0100

    Fix header component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,4 @@
+<div id="menu" class="bg-black">
+    <menu-dropdown
+        :dropdown="{label: 'Dropdown', options: [{label: 'Option 1', onClick: () => console.log('Option 1 clicked')},{label: 'Option 2', href: '#', classes: 'bg-red-500'}]}"></menu-dropdown>
+</div>
